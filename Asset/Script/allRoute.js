73678061719a3f9d2fd8e38fcbbdaffb7af5d3eb
Bastien Fras <fras.bastien@gmail.com>
2024-05-15 15:53:49 +0200

    Ajout de allRoute.js qui permet de créer un tableau avec toutes les routes de l'application et le nom du site

---
+++ Asset/Script/allRoute.js
@@ -0,0 +1,11 @@
+/**
+ * Crée un tableau contenant toutes les routes de l'application et définit la variablewebsiteName qui est le nom du site
+ */
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("../../", "../../Accueil", "../../pages/home.html"),];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
